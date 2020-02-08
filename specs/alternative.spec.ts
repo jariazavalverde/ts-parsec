450a1840f7eef7074d78acde352e7fa01d61a564
José Antonio Riaza Valverde <riazavalverde@gmail.com>
2020-02-08 20:10:58 +0100

    added tests for alternative interface

---
+++ specs/alternative.spec.ts
@@ -0,0 +1,31 @@
+import * as fc from "fast-check";
+import {Parser, pure} from "../src/parser";
+
+
+
+// Code under test
+const char: Parser<string> = new Parser(input => input.length > 0 ? [[input[0], input.substr(1)]] : []);
+
+
+
+// Properties
+
+// some v = (:) <$> v <*> many v
+test("v.some() == v.map(x => xs => [x].concat(xs)).seq(v.many())", () => {
+	fc.assert(
+		fc.property(fc.string(), input => {
+			let v = char;
+			expect(v.some().run(input)).toEqual(v.map(x => xs => [x].concat(xs)).seq(v.many()).run(input));
+		})
+	);
+});
+
+// many v = some v <|> pure []
+test("v.many() == v.some().or(pure([]))", () => {
+	fc.assert(
+		fc.property(fc.string(), input => {
+			let v = char;
+			expect(v.many().run(input)).toEqual(v.some().or(pure([])).run(input));
+		})
+	);
+});
