7e191db1d1466bed66481fecde9bf8c6e9470134
José Antonio Riaza Valverde <riazavalverde@gmail.com>
2020-02-14 03:47:18 +0100

    updated monad specs for new parsec

--- specs/monad.spec.ts
+++ specs/monad.spec.ts
@@ -1,16 +1,7 @@
 import * as fc from "fast-check";
-import {Parser} from "../src/parser";
-const {pure} = Parser;
-const {anyChar} = Parser.char;
-
-
-
-// Code under test
-function take(n: number): Parser<string> {
-	return new Parser(input => input.length >= n ? [[input.substr(0, n), input.substr(n)]] : []);
-}
-const even = (val: string): Parser<string> => val.charCodeAt(0) % 2 == 0 ? take(1) : take(2);
-const odd = (val: string): Parser<string> => val.charCodeAt(0) % 2 == 1 ? take(1) : take(2);
+import {Parsec, Char} from "../src/parsec";
+const {pure} = Parsec;
+const {anyChar, char} = Char;
 
 
 
@@ -19,9 +10,13 @@ const odd = (val: string): Parser<string> => val.charCodeAt(0) % 2 == 1 ? take(1
 // (left identity) return a >>= f == f a
 test("(left identity) pure(a).bind(f) == f(a)", () => {
 	fc.assert(
-		fc.property(fc.integer(), fc.string(), (a, input) => {
-			let f = take;
-			expect(pure(a).bind(f).run(input)).toEqual(f(a).run(input));
+		fc.property(fc.char(), fc.string(), (c, input) => {
+			let f = char;
+			expect(
+				pure(c).bind(f).runParser(undefined, "monad.spec.ts : left identity", c+input)
+			).toEqual(
+				f(c).runParser(undefined, "monad.spec.ts : left identity", c+input)
+			);
 		})
 	);
 });
@@ -31,7 +26,11 @@ test("(right identity) m.bind(pure) == m", () => {
 	fc.assert(
 		fc.property(fc.string(), input => {
 			let m = anyChar;
-			expect(m.bind(pure).run(input)).toEqual(m.run(input));
+			expect(
+				m.bind(pure).runParser(undefined, "monad.spec.ts : right identity", input)
+			).toEqual(
+				m.runParser(undefined, "monad.spec.ts : right identity", input)
+			);
 		})
 	);
 });
@@ -39,9 +38,13 @@ test("(right identity) m.bind(pure) == m", () => {
 // (associativity) (m >>= f) >>= g == m >>= (\x -> f x >>= g)
 test("(associativity) m.bind(f).bind(g) == m.bind(x => f(x).bind(g))", () => {
 	fc.assert(
-		fc.property(fc.string(), input => {
-			let m = anyChar, f = even, g = odd;
-			expect(m.bind(f).bind(g).run(input)).toEqual(m.bind(x => f(x).bind(g)).run(input));
+		fc.property(fc.char(), fc.string(), (c, input) => {
+			let m = anyChar, f = char, g = char;
+			expect(
+				m.bind(f).bind(g).runParser(undefined, "monad.spec.ts : associativity", c+c+c+input)
+			).toEqual(
+				m.bind((x: string) => f(x).bind(g)).runParser(undefined, "monad.spec.ts : associativity", c+c+c+input)
+			);
 		})
 	);
 });
